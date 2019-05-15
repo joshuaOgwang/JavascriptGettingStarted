409684374741efdae6abe6217d8db6895a955c49
JOSHUA OGWANG <joshuaogwang46@gmail.com>
2019-05-15 17:34:35 +0300

    added starter files

---
+++ blackJack/scripts.js
@@ -0,0 +1,13 @@
+//
+//Blackjack
+//project from Mark Zaymota on pluralsight
+//
+
+let card1 = "Ace of Spades",
+    card2 = "Tens of Hearts";
+
+console.log("Welcome to blackjack!");
+
+console.log("You are dealt: ");
+console.log(" " + card1);
+console.log(" " + card2);
